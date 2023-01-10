c93e9312d156f16b95f2adfa43b5c9f921d4864c
Baigho Fakku <baigho123@gmail.com>
2023-01-10 08:55:26 +0700

    - refactor 📦: install mdi

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomePage from "@/components/HomePage.vue";
 import RegisterPage from "@/components/RegisterPage.vue";
 import LoginPage from "@/components/Login/LoginPage.vue";
 import AdminPage from "@/components/Admin/AdminPage.vue";
+import FormPage from "@/components/Form/FormPage.vue";
 const routes = [
     {
         path: "/login",
@@ -23,6 +24,11 @@ const routes = [
         path: "/admintool",
         name: "AdminPage",
         component: AdminPage,
+    },
+    {
+        path: "/form",
+        name: "FormPage",
+        component: FormPage,
     },
 ];
 
